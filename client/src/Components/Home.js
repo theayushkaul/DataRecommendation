819026376c19bc2319c85f8a50fca6fb64013a88
BhavjotSingh-12 <114768116+BhavjotSingh-12@users.noreply.github.com>
2023-08-15 14:23:41 +0530

    Login

---
+++ client/src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import Upload from './Upload'
+
+
+const Home = () => {
+  return (
+  <div className="flex">
+<Login></Login>
+  </div>
+  )
+}
+
+export default Home
---
+++ client/src/Components/Login.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Login = () => {
+    {
+        return (
+          <div className="flex bg-slate-200 justify-center items-center h-screen outfit">
+            <div className="bg-white w-10/12 md:w-2/5 p-6 rounded-xl">
+            <span className="text-4xl">Login</span>
+            <form className="flex flex-col mt-6">
+              <label>Email</label>
+              <input className="focus:outline-none" type="text" placeholder="Enter your email..."/>
+              <label className="mt-6">Password</label>
+              <input className="focus:outline-none" type="password" placeholder="Enter your password..."/>
+              <button className="bg-gray-400 rounded-full mx-auto px-5 py-1 mt-6" type="submit">Login</button>
+            </form>
+            </div>
+          </div>
+        );
+      }
+}
+
+export default Login
